811c558bb42d1acaf35ca45a177ddd2da9996315
Colby Sprague <colbysprague@Colbys-MacBook-Pro-2.local>
2024-06-27 13:20:13 -0400

    Update kitten data

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
 catData = {
   "cats": [
     {
-      "name": "Fats Domino",
+      "name": "🎲 Fats Domino ",
       "imgPath": "assets/fatsdomino.jpeg",
-      "weight": "235g",
+      "weight": "288g",
       "desc": "I love to move around. I am a good walker"
     },
     {
-      "name": "Darth Vader",
+      "name": "🖤 Darth Vader",
       "imgPath": "assets/darthvader.jpeg",
-      "weight": "235g",
+      "weight": "282g",
       "desc": "I am very vocal. I am also a good sitter"
     },
     {
-      "name": "Fuzz Lightyear",
+      "name": "🚀 Fuzz Lightyear",
       "imgPath": "assets/fuzzlightyear.jpg",
-      "weight": "235g",
+      "weight": "253g",
       "desc": "I always have milk on my chin"
     },
     {
-      "name": "Godzilla",
+      "name": "🦍 Godzilla",
       "imgPath": "assets/godzilla.jpeg",
-      "weight": "235g",
+      "weight": "282g",
       "desc": "I am a nipple bully"
     },
     {
-      "name": "Bonk",
+      "name": "💥 Bonk",
       "imgPath": "assets/bunk.jpeg",
       "weight": "7.6lb",
       "desc": "I hate kittens"
     },
     {
-      "name": "Trixie",
+      "name": "🎀 Trixie (Mom)",
       "imgPath": "assets/mum.jpeg",
       "weight": "6.2lb",
       "desc": "I love my kittens"
