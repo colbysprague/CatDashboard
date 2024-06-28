2e6c59b905b4b667ae77769db68751a9405dfcff
Colby Sprague <colbysprague@Colbys-MacBook-Pro-2.local>
2024-06-28 10:03:00 -0400

    outline howOld reverse countdown

--- test.js
+++ test.js
@@ -1,18 +1,22 @@
-const kittenBirthday = new Date(2024, 5, 8); // June 8 2024
+const kittenBirthday = new Date(2024, 5, 8); // June 8, 2024
 
 function updateAge() {
-
-  console.log("Calling kitten age")
+  console.log("Calling kitten age");
 
   const now = new Date();
-  const ageInSeconds = (now - kittenBirthday) / 1000
+  const ageInSeconds = Math.floor((now - kittenBirthday) / 1000);
 
-  const ageInDays = Math.floor((ageInSeconds % (60 * 60 * 24 * 30.44)) / (60 * 60 * 24));
+  const seconds = ageInSeconds % 60;
+  const minutes = Math.floor((ageInSeconds / 60) % 60);
+  const hours = Math.floor((ageInSeconds / (60 * 60)) % 24);
+  const days = Math.floor((ageInSeconds / (60 * 60 * 24)) % 30.44);
+  const months = Math.floor((ageInSeconds / (60 * 60 * 24 * 30.44)) % 12);
+  const years = Math.floor(ageInSeconds / (60 * 60 * 24 * 365.25));
 
-  return ageInDays
-
-  // document.getElementbyId("kittenAge").textContent = `${ageInDays}`;
+  // document.getElementById("kittenAge").textContent =
+  console.log(`Y: ${years}, M: ${months}, D: ${days}, ` +
+    `H: ${hours}, M: ${minutes}, S: ${seconds}`);
 }
 
 updateAge();
-setInterval(updateAge, 1000)
+setInterval(updateAge, 1000);
