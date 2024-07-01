9e9bbbbdeb75192c2e29cf5d3f43af4a589e7cda
Colby Sprague <colbysprague@Colbys-MacBook-Pro-2.local>
2024-07-01 13:16:31 -0400

    adding weight updates

--- script.py
+++ script.py
@@ -1,12 +1,34 @@
+import datetime
 import json
 import pprint
 
+def calc_gain(weights):
+    if len(weights) >= 2:
+        return weights[-1] - weights[-2]
+
+
 # Specify the path to your JSON file
 json_file_path = 'kittenData.json'
+backup_file_path = 'backupKittenData.json'
 
 # Open the JSON file and read its contents
 with open(json_file_path, 'r') as file:
     json_data = json.load(file)
 
+# stash backup file
+with open(backup_file_path, 'w') as file:
+    json.dump(json_data, file, indent=2)
+
+# process new data
+print(f"Enter new weights for: ")
 for kitten in json_data:
-    print(kitten['name'])
+    if kitten.get("isKitten", False):
+        new_weight = int(input(f"   {kitten['name']}: "))
+        kitten.setdefault('weight', []).append(new_weight)
+        kitten['gain'] = calc_gain(kitten['weight'])
+        print(f"        + {kitten['gain']}")
+
+with open(json_file_path, 'w') as file:
+    json.dump(json_data, file, indent=2)
+
+print("Saving weight updates...")
